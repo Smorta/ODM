b857af4e0a68c0ccb2f0bbc781b8b9d51893586f
matteoruth <matteo.ruth@student.uliege.be>
2023-05-22 17:25:10 +0200

    REINFORCE

---
+++ project 3/REINFORCE_normal.py
@@ -0,0 +1,87 @@
+import matplotlib.pyplot as plt
+import numpy as np  
+import time
+import gymnasium as gym
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.autograd as autograd
+from torch.autograd import Variable
+
+from scipy.stats import multivariate_normal 
+
+def make_trajectory(theta, env, size=1000):
+   trajectory = []
+
+   mu = theta[:4]
+   Sigma = 1.5
+
+   s = env.reset()[0]
+   for _ in range(size):
+      action = np.array([multivariate_normal.rvs(mean = np.dot(mu, s), cov = Sigma)])
+
+      action = np.clip(action, -3, 3)
+      s, r, terminated, truncated, info = env.step(action)
+
+      epoch = [s, action, r]
+      trajectory.append(epoch)
+
+      if terminated or truncated:
+         break
+
+   return trajectory
+
+def main():
+   gamma = 0.99
+   alpha = 0.000025
+
+   theta = np.random.uniform(-3, 3, 4)
+   #theta[4] = 1.5
+  #theta = [-3.17348774,  8.74304374, -4.36935588, -3.19732884]
+   len_trajectory = []
+   Gs = []
+   mu1 = []
+   mu2 = []
+   mu3 = []
+   mu4 = []
+   #Sigmas = []
+   for i in tqdm(range(2000)):
+      env = gym.make("InvertedPendulum-v4", render_mode= None)
+      trajectory = make_trajectory(theta, env)
+      len_trajectory.append(len(trajectory))
+      mu1.append(theta[0])
+      mu2.append(theta[1])
+      mu3.append(theta[2])
+      mu4.append(theta[3])
+      #Sigmas.append(theta[4])
+
+      for t in range(len(trajectory)):
+         s1, s2, s3, s4 = trajectory[t][0]
+         a = trajectory[t][1]
+         r = trajectory[t][2]
+         G = 0
+         for k in range(t+1, len(trajectory)):
+            G += r * (gamma**(k-t-1)) 
+            r = trajectory[k][2]
+
+         mu = theta[:4] @ np.array([s1, s2, s3, s4])
+         Sigma = 1.5
+
+         gradient = np.array([s1, s2, s3, s4]) * ((a - mu) / (Sigma**2))
+         theta = theta + alpha*(gamma**t)*G*gradient
+      env.close()
+
+   plt.plot(len_trajectory, label = "len_trajectory")
+   plt.plot(Gs, label = "G")
+   plt.plot(mu1, label = "mu1")
+   plt.plot(mu2, label = "mu2")
+   plt.plot(mu3, label = "mu3")
+   plt.plot(mu4, label = "mu4")
+   #plt.plot(Sigmas, label = "Sigmas")
+   plt.legend()
+   plt.xlabel("epoch")
+   plt.ylabel("value")
+   plt.show()
+if __name__ == "__main__":
+    main()   
---
+++ project 3/REINFORCE_softmax.py
@@ -0,0 +1,79 @@
+import matplotlib.pyplot as plt
+import numpy as np  
+
+import gymnasium as gym
+from tqdm import tqdm
+
+EPOCHS = 10000
+ALPHA = 0.001
+GAMMA = 0.99
+#DISC_STEP = [2, 5, 10, 20]
+
+def make_trajectory(env, theta, size=1000):
+    trajectory = []
+    grad_log_pi = []
+    rewards = []
+    s = env.reset()[0]
+    for _ in range(size):
+        pi = policy(s, theta)
+        actions = np.linspace(-3, 3, DISC_STEP) # discrete actions
+        action = [np.random.choice(actions , p=pi)]
+
+        index = np.where(actions == action)[0][0]
+
+        next_s, r, terminated, truncated, info = env.step(action)
+        epoch = [s, action, r]
+        trajectory.append(epoch)
+
+        grad_pi = policy_grad(pi)[index,:] # grad of the action w.r.t. theta
+        grad_log_pi.append(s.reshape(4, 1).dot((grad_pi / pi[index])[None,:]))
+        rewards.append(r)
+
+        s = next_s
+        if terminated or truncated:
+            break
+    return trajectory, grad_log_pi, rewards
+   
+def policy(state, theta):
+    exp = np.exp(state.dot(theta)) # exp(theta^T * state)	
+    return exp/np.sum(exp) # softmax over all actions
+
+def policy_grad(policy):
+    temp = policy.reshape(-1,1)
+    return np.diagflat(temp) - np.dot(temp, temp.T)
+
+
+hyp_tune = []
+for DISC_STEP in [3, 5, 7, 9, 12]:
+    env = gym.make("InvertedPendulum-v4", render_mode= None)
+
+    theta = np.random.rand(4, DISC_STEP)
+    len_trajectory = []
+
+    for i in tqdm(range(EPOCHS)):
+        trajectory, grad_log_pi, rewards = make_trajectory(env, theta)
+        len_trajectory.append(len(trajectory))
+
+        for t in range(len(trajectory)):
+            theta = theta + ALPHA * grad_log_pi[t] * sum([ r * (GAMMA ** k) for k,r in enumerate(rewards[t:])])
+    
+    len_trajectory = np.array(len_trajectory)
+    len_trajectory = len_trajectory.reshape(-1, 100)
+    len_trajectory = np.mean(len_trajectory, axis=1)
+    hyp_tune.append(len_trajectory)
+# Calculate the mean along the second axis (column-wise) to get the reduced array
+
+plt.figure()
+plt.plot(hyp_tune[0], label='3')
+plt.plot(hyp_tune[1], label='5')
+plt.plot(hyp_tune[2], label='7')
+plt.plot(hyp_tune[3], label='9')
+plt.plot(hyp_tune[4], label='12')
+plt.grid()
+plt.xlabel('Epochs', fontsize=16)
+plt.ylabel('Mean Trajectory Length', fontsize=16)
+plt.text(0.9, -0.1, "x100", transform=plt.gca().transAxes, fontsize=10)
+plt.legend()
+plt.show()
+
+    
