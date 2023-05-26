c73c86e2452a188af23b4c1bda007266626912ab
matteoruth <matteo.ruth@student.uliege.be>
2023-05-26 17:14:46 +0200

    plots, lot of plots

--- project 3/REINFORCE_normal.py
+++ project 3/REINFORCE_normal.py
@@ -11,11 +11,11 @@ from torch.autograd import Variable
 
 from scipy.stats import multivariate_normal 
 
-def make_trajectory(theta, env, size=1000):
+def make_trajectory(theta, env, size=200):
    trajectory = []
 
    mu = theta[:4]
-   Sigma = 1.5
+   Sigma = 0.8
 
    s = env.reset()[0]
    for _ in range(size):
@@ -34,19 +34,20 @@ def make_trajectory(theta, env, size=1000):
 
 def main():
    gamma = 0.99
-   alpha = 0.000025
+   alpha = 0.00001
 
    theta = np.random.uniform(-3, 3, 4)
-   #theta[4] = 1.5
-  #theta = [-3.17348774,  8.74304374, -4.36935588, -3.19732884]
+
+   #theta = [-3.17348774,  8.74304374, -4.36935588, -3.19732884, 0.5]
    len_trajectory = []
+   hyp_tune = []
    Gs = []
    mu1 = []
    mu2 = []
    mu3 = []
    mu4 = []
    #Sigmas = []
-   for i in tqdm(range(2000)):
+   for i in tqdm(range(10000)):
       env = gym.make("InvertedPendulum-v4", render_mode= None)
       trajectory = make_trajectory(theta, env)
       len_trajectory.append(len(trajectory))
@@ -61,27 +62,35 @@ def main():
          a = trajectory[t][1]
          r = trajectory[t][2]
          G = 0
-         for k in range(t+1, len(trajectory)):
-            G += r * (gamma**(k-t-1)) 
+         for k in range(t, len(trajectory)):
+            G += r * (gamma**(k-t)) 
             r = trajectory[k][2]
 
          mu = theta[:4] @ np.array([s1, s2, s3, s4])
-         Sigma = 1.5
+         Sigma = 0.8
 
          gradient = np.array([s1, s2, s3, s4]) * ((a - mu) / (Sigma**2))
          theta = theta + alpha*(gamma**t)*G*gradient
       env.close()
 
-   plt.plot(len_trajectory, label = "len_trajectory")
-   plt.plot(Gs, label = "G")
-   plt.plot(mu1, label = "mu1")
-   plt.plot(mu2, label = "mu2")
-   plt.plot(mu3, label = "mu3")
-   plt.plot(mu4, label = "mu4")
-   #plt.plot(Sigmas, label = "Sigmas")
+   len_trajectory = np.array(len_trajectory)
+   len_trajectory = len_trajectory.reshape(-1, 100)
+   len_trajectory = np.mean(len_trajectory, axis=1)
+   hyp_tune.append(len_trajectory)
+   #plt.plot(len_trajectory, label = "len_trajectory")
+   #plt.plot(Gs, label = "G")
+   # plt.plot(mu1, label = "mu1")
+   # plt.plot(mu2, label = "mu2")
+   # plt.plot(mu3, label = "mu3")
+   # plt.plot(mu4, label = "mu4")
+   #plt.plot(Sigmas, label = "Sigma")
+   plt.plot(hyp_tune[0])
+   plt.grid()
    plt.legend()
    plt.xlabel("epoch")
-   plt.ylabel("value")
+   plt.ylabel("Mean Trajectory Length")
+   plt.text(0.9, -0.1, "x100", transform=plt.gca().transAxes, fontsize=10)
+   plt.savefig("normal_reinforce_no_sigma_len_dist.pdf")
    plt.show()
 if __name__ == "__main__":
     main()   
