fe78d53e6a1cfa117d13e185578dc9876af3e211
matteoruth <matteo.ruth@student.uliege.be>
2023-05-26 18:14:46 +0200

    saved theta

--- project 3/REINFORCE_softmax.py
+++ project 3/REINFORCE_softmax.py
@@ -7,7 +7,6 @@ from tqdm import tqdm
 EPOCHS = 10000
 ALPHA = 0.001
 GAMMA = 0.99
-#DISC_STEP = [2, 5, 10, 20]
 
 def make_trajectory(env, theta, size=200):
     trajectory = []
@@ -16,9 +15,8 @@ def make_trajectory(env, theta, size=200):
     s = env.reset()[0]
     for _ in range(size):
         pi = policy(s, theta)
-        #actions = np.linspace(-3, 3, DISC_STEP) # discrete actions
-        #actions = np.array([-3, -2, -1.5, -1, -0.75, -0.5, -0.25, -0.1, 0, 0.1, 0.25, 0.5, 0.75, 1, 1.5, 2, 3])
-        actions = np.array([-3, -1.5, -0.75, -0.1, 0, 0.1, 0.75, 1.5, 3])
+        actions = np.linspace(-3, 3, DISC_STEP) # discrete actions
+
 
         action = [np.random.choice(actions , p=pi)]
 
@@ -46,10 +44,10 @@ def policy_grad(policy):
 
 
 hyp_tune = []
-for DISC_STEP in [3]:
+for DISC_STEP in [5]:
     env = gym.make("InvertedPendulum-v4", render_mode= None)
 
-    theta = np.random.rand(4, 9)
+    theta = np.random.rand(4, DISC_STEP)
     len_trajectory = []
 
     for i in tqdm(range(EPOCHS)):
@@ -59,25 +57,27 @@ for DISC_STEP in [3]:
         for t in range(len(trajectory)):
             theta = theta + ALPHA * grad_log_pi[t] * sum([ r * (GAMMA ** k) for k,r in enumerate(rewards[t:])])
     
-    len_trajectory = np.array(len_trajectory)
-    len_trajectory = len_trajectory.reshape(-1, 100)
-    len_trajectory = np.mean(len_trajectory, axis=1)
-    hyp_tune.append(len_trajectory)
-# Calculate the mean along the second axis (column-wise) to get the reduced array
+    np.save('theta_simple_pendulum.npy', theta) 
 
-plt.figure()
-plt.plot(hyp_tune[0], label='Custom discretization')
-# plt.plot(hyp_tune[0], label='3')
-# plt.plot(hyp_tune[1], label='5')
-# plt.plot(hyp_tune[2], label='7')
-# plt.plot(hyp_tune[3], label='9')
-# plt.plot(hyp_tune[4], label='12')
-plt.grid()
-plt.xlabel('epoch')
-plt.ylabel('Mean Trajectory Length')
-plt.text(0.9, -0.1, "x100", transform=plt.gca().transAxes, fontsize=10)
-plt.legend()
-plt.savefig('REINFORCE_softmax_disc-cust.png')
-plt.show()
+
+    # len_trajectory = np.array(len_trajectory)
+    # len_trajectory = len_trajectory.reshape(-1, 100)
+    # len_trajectory = np.mean(len_trajectory, axis=1)
+    # hyp_tune.append(len_trajectory)
+
+# plt.figure()
+# plt.plot(hyp_tune[0], label='Custom discretization')
+# # plt.plot(hyp_tune[0], label='3')
+# # plt.plot(hyp_tune[1], label='5')
+# # plt.plot(hyp_tune[2], label='7')
+# # plt.plot(hyp_tune[3], label='9')
+# # plt.plot(hyp_tune[4], label='12')
+# plt.grid()
+# plt.xlabel('epoch')
+# plt.ylabel('Mean Trajectory Length')
+# plt.text(0.9, -0.1, "x100", transform=plt.gca().transAxes, fontsize=10)
+# plt.legend()
+# plt.savefig('REINFORCE_softmax_disc-cust.png')
+# plt.show()
 
     
