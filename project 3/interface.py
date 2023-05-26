eef3c0e9c0f2e2881a30b64d9f0589795f14a2d6
matteoruth <matteo.ruth@student.uliege.be>
2023-05-26 21:31:36 +0200

    updated interface

--- project 3/interface.py
+++ project 3/interface.py
@@ -4,6 +4,7 @@ import pickle
 import sys
 import pandas as pd
 import getopt
+import os
 import argparse
 from matplotlib import pyplot as plt
 from matplotlib import animation
@@ -24,11 +25,20 @@ if __name__ == "__main__":
         parser.add_argument("algo")
         parser.add_argument("Nbr_action")
         parser.add_argument("model_path")
+        parser.add_argument("expected_reward")
         args = parser.parse_args()
         env_name = args.env
         algo = args.algo
         Nbr_action = int(args.Nbr_action)
         model_path = args.model_path
+
+        if args.expected_reward == "True":
+            expected_reward = True
+        elif args.expected_reward == "False":
+            expected_reward = False
+        else:
+            print(arg_help)
+            sys.exit(2)
 
     except:
         print(arg_help)
@@ -56,45 +66,89 @@ if __name__ == "__main__":
         print("Total step: ", step)
 
     elif algo == 'softmax_simple':
-        theta = np.load("theta_simple_pendulum_30000.npy")
-        env = gym.make("InvertedPendulum-v4", render_mode="human")
-        s = env.reset()[0]
-
-        step = 0
-        G = 0
-        while True: 
-            pi = policy(s, theta)
-            actions = np.linspace(-3, 3, 5)
-            action = [np.random.choice(actions , p=pi)]
-            next_s, r, terminated, truncated, info = env.step(action)
-            s = next_s
-            G += r
-            step += 1
-            if terminated or truncated:
-                break
-        print("Total reward: ", G)
-        print("Total step: ", step)
+        current_directory = os.getcwd()
+        file_path = os.path.join(current_directory, "project 3", "theta_simple_pendulum_30000.npy")
+        theta = np.load(file_path)
+        
+        if expected_reward:
+            values = []
+            for _ in range(50):
+                env = gym.make("InvertedPendulum-v4", render_mode= None)
+                s = env.reset()[0]
+                
+                step = 0
+                G = 0
+                while True: 
+                    pi = policy(s, theta)
+                    actions = np.linspace(-3, 3, 5)
+                    action = [np.random.choice(actions , p=pi)]
+                    next_s, r, terminated, truncated, info = env.step(action)
+                    s = next_s
+                    G += r
+                    step += 1
+                
+                    if terminated or truncated:
+                        break
+                values.append(G)
+            print(f"The expected reward is : {np.mean(values)}")
+        else:
+            env = gym.make("InvertedPendulum-v4", render_mode="human")
+            s = env.reset()[0]
+            step = 0
+            G = 0
+            while True: 
+                pi = policy(s, theta)
+                actions = np.linspace(-3, 3, 5)
+                action = [np.random.choice(actions , p=pi)]
+                next_s, r, terminated, truncated, info = env.step(action)
+                s = next_s
+                G += r
+                step += 1
+                if terminated or truncated:
+                    break
+            print("Total reward: ", G)
+            print("Total step: ", step)
 
     elif algo == 'softmax_double':
-        theta = np.load("theta_double_pendulum_30000.npy")
-        env = gym.make("InvertedDoublePendulum-v4", render_mode="human")
-        s = env.reset()[0]
-
-        step = 0
-        G = 0
-        print(theta)
-        while True: 
-            pi = policy(s, theta)
-            actions = np.linspace(-3, 3, 9)
-            action = [np.random.choice(actions , p=pi)]
-            next_s, r, terminated, truncated, info = env.step(action)
-            s = next_s
-            G += r
-            step += 1
-            if terminated or truncated:
-                break
-        print("Total reward: ", G)
-        print("Total step: ", step)
+        current_directory = os.getcwd()
+        file_path = os.path.join(current_directory, "project 3", "theta_double_pendulum_100000.npy")
+        theta = np.load(file_path)
+        if expected_reward:
+            values = []
+            for _ in range(50):
+                env = gym.make("InvertedDoublePendulum-v4", render_mode= None)
+                s = env.reset()[0]
+                step = 0
+                G = 0
+                while True: 
+                    pi = policy(s, theta)
+                    actions = np.linspace(-3, 3, 9)
+                    action = [np.random.choice(actions , p=pi)]
+                    next_s, r, terminated, truncated, info = env.step(action)
+                    s = next_s
+                    G += r
+                    step += 1
+                    if terminated or truncated:
+                        break
+                values.append(G)
+            print(f"The expected reward is : {np.mean(values)}")
+        else:
+            env = gym.make("InvertedDoublePendulum-v4", render_mode="human")
+            s = env.reset()[0]
+            step = 0
+            G = 0
+            while True: 
+                pi = policy(s, theta)
+                actions = np.linspace(-3, 3, 9)
+                action = [np.random.choice(actions , p=pi)]
+                next_s, r, terminated, truncated, info = env.step(action)
+                s = next_s
+                G += r
+                step += 1
+                if terminated or truncated:
+                    break
+            print("Total reward: ", G)
+            print("Total step: ", step)
 
     elif algo == 'DDPG':
         pass
